540f4b191264ce67e78c3c0eea6ff563e1f73721
125cc-Wine <lavidconsultora@gmail.com>
2026-03-16 19:03:42 -0300

    Update guardar-puntuacion.js

--- api/guardar-puntuacion.js
+++ api/guardar-puntuacion.js
@@ -1,1 +1,39 @@
-
+// Sign with RS256 using Web Crypto
+  const keyData = pemToArrayBuffer(privateKey);
+  const cryptoKey = await crypto.subtle.importKey(
+    "pkcs8", keyData,
+    { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" },
+    false, ["sign"]
+  );
+  const signature = await crypto.subtle.sign("RSASSA-PKCS1-v1_5", cryptoKey, new TextEncoder().encode(unsigned));
+  const jwt = `${unsigned}.${b64url(signature)}`;
+ 
+  // Exchange JWT for access token
+  const tokenRes = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: `grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=${jwt}`,
+  });
+  const tokenData = await tokenRes.json();
+  if (!tokenData.access_token) throw new Error("No se pudo obtener token de Google: " + JSON.stringify(tokenData));
+  return tokenData.access_token;
+}
+ 
+function b64url(data) {
+  let str;
+  if (data instanceof ArrayBuffer) {
+    str = String.fromCharCode(...new Uint8Array(data));
+  } else {
+    str = typeof data === "string" ? data : JSON.stringify(data);
+  }
+  return btoa(str).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+}
+ 
+function pemToArrayBuffer(pem) {
+  const b64 = pem.replace(/-----BEGIN PRIVATE KEY-----|-----END PRIVATE KEY-----|\n/g, "");
+  const binary = atob(b64);
+  const buf = new ArrayBuffer(binary.length);
+  const view = new Uint8Array(buf);
+  for (let i = 0; i < binary.length; i++) view[i] = binary.charCodeAt(i);
+  return buf;
+}
